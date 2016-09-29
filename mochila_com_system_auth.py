5657efe53aacb792eb39e05e42d34dfd9e2bd762
EdwardRamos2 <eduardo.linux.root@gmail.com>
2016-09-28 20:43:44 -0400

    Update mochila_com_system_auth.py

--- mochila_com_system_auth.py
+++ mochila_com_system_auth.py
@@ -18,81 +18,89 @@ elif user in db_user and pass1 in db_pass:
     senha = db_pass.index(pass1)
     if usuario == senha:
         print("Usuario e Senha corretos")
+
+
+        def mochila2():
+                if 0 == 0:
+                    print("Itens da mochila a serem inseridos:  \n")
+                    print("Insert o 1 produto! Categoria higiene:\n")
+                    item_1 = str(raw_input("Insert produto!:"))
+                if item_1 != 0:
+                    print
+                    print("(+) Produto inserido com sucesso!\n")
+                else:
+                    print("Next")
+
+                print("Insert o 2 produto! Categora roupa:\n")
+                item_2 = str(raw_input("Insert produto!:\n"))
+                if item_2 != 0:
+                    print("(+) Produto inserido com sucesso!\n")
+                else:
+                    print("Next")
+
+                print("Insert o 3 produto! Categoria acessorios:\n")
+                item_3 = str(raw_input("Insert produto!:\n"))
+                if item_3 != 0:
+                    print("(+) Produto inserido com sucesso!\n")
+                else:
+                    print("Next")
+
+                print("Produtos inseridos na mochila:\n")
+                print("CATEGORIA: HIGIENE")
+                print(item_1)
+                print("")
+                print("CATEGORIA: ROUPA")
+                print(item_2)
+                print("")
+                print("CATEGORIA: ACESSORIOS")
+                print(item_3)
+                print("")
+
+
+                print("TODOS PRODUTOS INSERIDOS")
+
+        print(mochila2())
+
+        def adicional():
+                print("Produtos adicionais:")
+                print("Categoria: ALIMENTO - LANCHE\n")
+
+                alimento = str(raw_input("Insert 1 ALIMENTO"))
+                if alimento != 0:
+                    print("(+) Alimento inserido com sucesso!\n'")
+                else:
+                    print("Next")
+
+                lanche = str(raw_input("Insert 1 LANCHE"))
+                if lanche != 0:
+                    print("(+) Lanche insrido com sucesso!\n")
+                else:
+                    print("Next")
+
+                print("Lanche inserido com sucesso!\n'")
+
+                print("Produtos adicionais inseridos:")
+                print("CATEGORIA: ALIMENTO")
+                print(alimento)
+
+                print("CATEGORIA: LANCHE")
+                print(lanche)
+                print("")
+                print("PRODUTOS ADICIONAIS INSERIDOS")
+
+        print(adicional())
+
     else:
         print("Usuario ou Senha nao corresponde")
 
 
-def mochila2():
-        if 0 == 0:
-            print("Itens da mochila a serem inseridos:  \n")
-            print("Insert o 1 produto! Categoria higiene:\n")
-            item_1 = str(raw_input("Insert produto!:"))
-        if item_1 != 0:
-            print
-            print("(+) Produto inserido com sucesso!\n")
-        else:
-            print("Next")
-
-        print("Insert o 2 produto! Categora roupa:\n")
-        item_2 = str(raw_input("Insert produto!:\n"))
-        if item_2 != 0:
-            print("(+) Produto inserido com sucesso!\n")
-        else:
-            print("Next")
-
-        print("Insert o 3 produto! Categoria acessorios:\n")
-        item_3 = str(raw_input("Insert produto!:\n"))
-        if item_3 != 0:
-            print("(+) Produto inserido com sucesso!\n")
-        else:
-            print("Next")
-
-        print("Produtos inseridos na mochila:\n")
-        print("CATEGORIA: HIGIENE")
-        print(item_1)
-        print("")
-        print("CATEGORIA: ROUPA")
-        print(item_2)
-        print("")
-        print("CATEGORIA: ACESSORIOS")
-        print(item_3)
-        print("")
 
 
-        print("TODOS PRODUTOS INSERIDOS")
 
-print(mochila2())
 
-def adicional():
-        print("Produtos adicionais:")
-        print("Categoria: ALIMENTO - LANCHE\n")
 
-        alimento = str(raw_input("Insert 1 ALIMENTO"))
-        if alimento != 0:
-            print("(+) Alimento inserido com sucesso!\n'")
-        else:
-            print("Next")
 
-        lanche = str(raw_input("Insert 1 LANCHE"))
-        if lanche != 0:
-            print("(+) Lanche insrido com sucesso!\n")
-        else:
-            print("Next")
 
-        print("Lanche inserido com sucesso!\n'")
-
-        print("Produtos adicionais inseridos:")
-        print("CATEGORIA: ALIMENTO")
-        print(alimento)
-
-        print("CATEGORIA: LANCHE")
-        print(lanche)
-        print("")
-        print("PRODUTOS ADICIONAIS INSERIDOS")
-
-print(adicional())
-
-print("The end")
 
 
 
